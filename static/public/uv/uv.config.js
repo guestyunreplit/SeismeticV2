1c504b703af01196b9d80c5bc3f2da76a91b4456
guestyunreplit <131507868+guestyunreplit@users.noreply.github.com>
2023-04-21 20:10:20 -0800

    Add files via upload

---
+++ static/public/uv/uv.config.js
@@ -0,0 +1,17 @@
+// This file overwrites the stock UV config.js
+  window.addEventListener('beforeunload', function(e) {
+  e.preventDefault();
+  e.returnValue = '';
+});
+
+self.__uv$config = {
+  prefix: "/uv/service/",
+  bare: "/bare/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
